e2b5106f11616de338d31eed70ced8036925406f
RohmatWorkstar <rohmatworkstar99@gmail.com>
2024-11-07 12:35:57 +0700

    Initial commit

---
+++ src/sections/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+    return (
+        <div>
+            <nav>
+                <a href="">Home</a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </nav>
+        </div>
+    );
+};
